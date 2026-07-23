b7e298326ce34f626659cca99a1fe15918c0d695
vesa-fioni <vesa.lindqvist@fioni.fi>
2026-07-23 11:56:32 +0300

    UI fix

--- shared.js
+++ shared.js
@@ -464,7 +464,7 @@ function startListeningToGroup(db, cfg) {
 
 // Näytetään ylärivillä, jotta näet onko selaimessa uusin versio.
 // Kasvata tätä JA index.html:n shared.js?v=N -numeroa aina kun tiedostoa muutetaan.
-const APP_VERSION = "v18";
+const APP_VERSION = "v19";
 
 function boot() {
   const versionEl = document.getElementById("appVersion");
