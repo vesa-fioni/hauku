eaa2163ad64ab84442fdaf9f98d86d1d96234532
vesa-fioni <vesa.lindqvist@fioni.fi>
2026-07-31 15:08:55 +0300

    Location to build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation("com.google.android.gms:play-services-location:21.4.0")
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
